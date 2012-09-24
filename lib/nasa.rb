9db33f9b682e0c3ba03a01a85b653caa411b7c6e
Kaushik Thirthappa <iamkaushikkumar@gmail.com>
2012-09-24 19:50:16 +0530

    added search data by id and slug

--- lib/nasa.rb
+++ lib/nasa.rb
@@ -1,12 +1,21 @@
 require "nasa/version"
-require "nasa/nasa"
 require "mechanize"
 require "json"
 
 module Nasa
 
+  # Get the latest feed published on http:://data.nasa.gov
+  # you will get 10 feeds by default.
   def self.get_latest_data(count=10)
     JSON.parse( Mechanize.new.get( self.url + "/get_recent_datasets?count=#{count}").body )
+  end
+
+  def self.search_data_by_id(id)
+    JSON.parse( Mechanize.new.get( self.url + "/get_dataset?id=#{id.to_i}").body )
+  end
+
+  def self.search_data_by_slug(slug)
+    JSON.parse( Mechanize.new.get( self.url + "/get_dataset?slug=#{slug.to_s}").body )
   end
 
   def self.url
